0dba5969e9b23fba1437232931771f4cf9ffef02
Aidan Root <root.aidan@gmail.com>
2024-09-11 12:18:53 -0400

    Revert "Update game.py"
    This reverts commit c17cfd4ca7796c48e3ab3dac1e9cfa7fee25660f.

--- game.py
+++ game.py
@@ -6,8 +6,7 @@ def ask_question(question, answer):
 
 def main():
     questions = {
-        #changed first question
-        "What is the new word people use instead of 'cool'": "skibidi",
+        "What is the capital of France?": "paris",
         "What is 2 + 2?": "4",
         "What is the color of the sky?": "blue",
         "Who wrote 'To Kill a Mockingbird'?": "harper lee",
